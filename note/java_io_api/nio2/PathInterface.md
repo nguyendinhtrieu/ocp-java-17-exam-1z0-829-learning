831d2e29d5cfe8e8255395f42cbafd97a25931e4
nguyendinhtrieu <nguyendinhtrieu365@gmail.com>
2023-09-24 22:52:25 +0700

    Note for PathInterface

--- note/java_io_api/nio2/PathInterface.md
+++ note/java_io_api/nio2/PathInterface.md
@@ -25,3 +25,31 @@ other - the path to resolve against this path
 
 Returns:
 the resulting path
+
+-------------------------------------------------------------
+Path getRoot()<br>
+Returns the root component of this path as a Path object, or null if this path does not have a root component.
+
+Path getName(int index)<br>
+Returns a name element of this path as a Path object.<br>
+The index parameter is the index of the name element to return. The element that is closest to the root in the directory hierarchy has index 0. The element that is farthest from the root has index count-1.
+
+Parameters:<br>
+index - the index of the element
+
+The following 4 points about Path.getName() method are good to know as well:
+
+1. Indices for path names start from 0. 
+2. Root (i.e. c:\) is not included in path names. 
+3. \ is NOT a part of a path name. 
+4. If you pass a negative index or a value greater than or equal to the number of elements, or this path has zero name elements, java.lang.IllegalArgumentException is thrown. It DOES NOT return null.
+
+Thus, for example, If your Path is "c:\\code\\java\\PathTest.java", 
+
+```
+p1.getRoot()  is c:\  ((For Unix based environments, the root is usually / ). 
+p1.getName(0)  is code 
+p1.getName(1)  is java 
+p1.getName(2)  is PathTest.java
+p1.getName(3)  will cause IllegalArgumentException to be thrown.
+```
--- src/test/java_ooa/overloading/Test.java
+++ src/test/java_ooa/overloading/Test.java
@@ -1,5 +1,13 @@
 package test.java_ooa.overloading;
 
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 class Point {
     int x, y;
 }
@@ -21,8 +29,113 @@ class Test {
         System.out.println("(Point, ColoredPoint)");
     }
 
+    public static int i = 0;
+
+    public void t() {
+        this.i = 9;
+    }
+
     public static void main(String[] args) {
+        int i, i1 = 0;
+        char a = 'a', b = 98;
         ColoredPoint cp = new ColoredPoint();
         test(cp, cp);
     }
-} 
+
+}
+
+class A {
+}
+
+class B extends A {
+}
+
+class C extends B {
+}
+
+class Base {
+    public List<B> transform(List<B> list) {
+        return null;
+    }
+}
+
+class Derived extends Base {
+    public ArrayList<B> transform(List<B> list) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Runnable r = () -> System.out.println("In Runnable");
+        Callable<Integer> c = () -> {
+            System.out.println("In Callable");
+            return 0;
+        };
+        var es = Executors.newCachedThreadPool();          //INSERT CODE HERE        
+        Future<?> submit = es.submit(r);
+        Future<Integer> submit1 = es.submit(c);
+        es.shutdown();
+    }
+
+}
+
+
+abstract interface i {
+    public final static int i = 0;
+    private static void m() {}
+    public default void m1() {}
+    public void m2();
+}
+
+abstract class AA {
+    protected final static int i = 0;
+    private static void m() {}
+    private void m1() {}
+    protected abstract void m2();
+}
+
+interface I1 {
+    public default void method() {
+        System.out.println("I1");
+    }
+}
+
+interface I2 extends I1 {
+    public default void method() {
+        System.out.println("I2");
+    }
+}
+
+class C11 implements I1, I2 {
+
+    //    @Override
+//    public void method() {
+//        I2.super.method();
+//    }
+    public static void main(String[] args) {
+        new C11().method();
+    }
+
+}
+
+class XX {
+    public int m(int i){return 1;}
+}
+class XXX extends XX {
+    public int m(Integer i) {
+        return 2;
+    }
+}
+
+class X {
+    public static void main(String[] args) throws Exception {
+        try (var fis = new FileInputStream("/.../t.txt"); var isr = new InputStreamReader(fis)) {
+            while (isr.ready()) {
+                isr.skip(1);
+                int i = isr.read();
+                char c = (char) i;
+                System.out.println(i);
+//                System.out.println((int)c);
+            }
+        }
+    }
+}
